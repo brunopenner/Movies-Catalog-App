571b3b522c4b19f30994b720608fbf9f76ff4e2d
Bruno Penner <brunopenner01@gmail.com>
2024-12-16 11:33:53 +0800

    Passing movie data onto Movie component

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -2,6 +2,12 @@ import "./Movie.css"
 
 export default function Movie ({movie}) {
     return (
-        <p>Movie Component</p>
+        <div className="Movie">
+            <img src={movie.Poster} alt={movie.Title} />
+            <div className="movieContent">
+                <h2>{movie.Title}</h2>
+                <p>📅 {movie.Year}</p>
+            </div>
+        </div>
     )
 }
