20bbffc0badc2d675e512b56690eb038c2c2ffff
Bruno Penner <brunopenner01@gmail.com>
2024-12-18 09:23:12 +0800

    Creating Star Rating component

--- src/MovieRating.jsx
+++ src/MovieRating.jsx
@@ -1,5 +1,31 @@
+import { Star, StarOutline } from "@mui/icons-material"
+import "./MovieRating.css"
+import { useState } from "react"
+
 export default function MovieRating () {
+    const [starRating, setStarRating] = useState(0);
+    const [hoverRating, setHoverRating] = useState(0);
+
+    const renderStars = () => {
+        const stars = []
+        let hasRating = false;
+        for (let i=1; i<=10; i++) {
+            stars.push(
+                <span key={i} className="star"
+                    onClick={ () => setStarRating(i)}
+                    onMouseEnter={() => setHoverRating(i)}
+                    onMouseLeave={() => setHoverRating(0)}
+                >
+                    {i <= (hoverRating || starRating) ? <Star /> : <StarOutline />}
+                </span>
+            )
+        }
+        return stars;
+    }
     return (
-        <p>Movie Rating Component</p>
+        <div className="movieRating">
+            {renderStars()}
+            {starRating > 0 ? "Button here" : ""}
+        </div>
     )
 }
